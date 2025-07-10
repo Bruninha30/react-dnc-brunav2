8fc080020c76b059e40ba81fa11491d247bf56cb
Bruna Rodrigues <brunasp3008@gmail.com>
2025-07-10 13:02:21 -0400

    corrigindo o vercel

--- react-dnc-brunav2/vite.config.js
+++ react-dnc-brunav2/vite.config.js
@@ -1,7 +1,17 @@
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  base: '/', // 👈 aqui tá no lugar certo!
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    fs: {
+      allow: ['.']
+    }
+  }
 })
