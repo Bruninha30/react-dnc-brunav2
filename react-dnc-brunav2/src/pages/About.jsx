e13f6a2706af0ca566725b1ff04fedb475875718
Bruna Rodrigues <brunasp3008@gmail.com>
2025-07-10 10:43:15 -0400

    first commit

---
+++ react-dnc-brunav2/src/pages/About.jsx
@@ -0,0 +1,25 @@
+import AboutText from '../components/AboutText/AboutText';
+import Banner from '../components/Banner/Banner';
+import Header from '../components/Header/Header';
+import Footer from '../components/footer/footer'; 
+import { useContext } from 'react';
+
+// context
+import { AppContext } from '../contexts/AppContext';
+
+function About() {
+  const appContext = useContext(AppContext);
+
+  return (
+    <>
+      <Header />
+      <Banner title= {appContext.languages[appContext.language].menu.about} image="about.jpg" />
+      <div className="container">
+        <AboutText />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default About;
